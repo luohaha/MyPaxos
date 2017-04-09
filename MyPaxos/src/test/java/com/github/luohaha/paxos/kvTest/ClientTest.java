39a50eddc37c2ff8665202cbad576ec19d3f7a6d
Yixin Luo <18810541851@163.com>
2017-04-09 21:55:11 +0800

    添加测试

--- MyPaxos/src/test/java/com/github/luohaha/paxos/kvTest/ClientTest.java
+++ MyPaxos/src/test/java/com/github/luohaha/paxos/kvTest/ClientTest.java
@@ -13,8 +13,8 @@ public class ClientTest {
 	public static void main(String[] args) {
 		MyPaxosClient client = new MyPaxosClient("localhost", 33333);
 		try {
-			//client.submit(new Gson().toJson(new MsgBean("put", "name", "Mike")), 1);
-			//client.submit(new Gson().toJson(new MsgBean("put", "name", "Neo")), 2);
+			client.submit(new Gson().toJson(new MsgBean("put", "name", "Mike")), 1);
+			client.submit(new Gson().toJson(new MsgBean("put", "name", "Neo")), 2);
 			//client.submit(new Gson().toJson(new MsgBean("delete", "name", "")), 2);
 			client.submit(new Gson().toJson(new MsgBean("get", "name", "")), 1);
 		} catch (IOException e) {
