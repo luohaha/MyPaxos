e786ffb12bd30cd6d55656b44ddd63b754962481
Yixin Luo <18810541851@163.com>
2017-04-15 11:51:53 +0800

    日志操作接口

--- MyPaxos/src/main/java/com/github/luohaha/paxos/log/MyPaxosLog.java
+++ MyPaxos/src/main/java/com/github/luohaha/paxos/log/MyPaxosLog.java
@@ -1,37 +1,36 @@
 package com.github.luohaha.paxos.log;
 
+import java.io.IOException;
+
 import com.github.luohaha.paxos.core.Accepter;
 
 public interface MyPaxosLog {
 	/**
 	 * 获取log文件的位置
-	 * @param accepter
-	 * 对应的accepter
 	 * @return
 	 * 返回文件位置
 	 */
-	public String getLogFileAddr(Accepter accepter);
+	public String getLogFileAddr();
 	/**
 	 * 从log文件中恢复
-	 * @param logFileAddr
 	 */
-	public void recoverFromLog(Accepter accepter, String logFileAddr);
+	public void recoverFromLog() throws IOException;
 	/**
 	 * 每次新增，修改ballot都会调用此函数，将记录加入log
 	 * @param instanceId
 	 * @param ballot
 	 */
-	public void setInstanceBallot(int instanceId, int ballot);
+	public void setInstanceBallot(int instanceId, int ballot) throws IOException;
 	/**
 	 * 每次新增，修改acceptedBallot都会调用此函数，将记录加入log
 	 * @param instanceId
 	 * @param acceptedBallot
 	 */
-	public void setInstanceAcceptedBallot(int instanceId, int acceptedBallot);
+	public void setInstanceAcceptedBallot(int instanceId, int acceptedBallot) throws IOException;
 	/**
 	 * 每次新增，修改value都会调用此函数，将记录加入log
 	 * @param instanceId
 	 * @param value
 	 */
-	public void setInstanceValue(int instanceId, Object value);
+	public void setInstanceValue(int instanceId, Object value) throws IOException;
 }
