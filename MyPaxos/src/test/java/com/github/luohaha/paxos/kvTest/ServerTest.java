45b13d1062bf8818cf98a84314ca4e51704d05c7
Yixin Luo <18810541851@163.com>
2017-04-29 21:57:28 +0800

    update test

--- MyPaxos/src/test/java/com/github/luohaha/paxos/kvTest/ServerTest.java
+++ MyPaxos/src/test/java/com/github/luohaha/paxos/kvTest/ServerTest.java
@@ -6,10 +6,10 @@ import com.github.luohaha.paxos.main.MyPaxos;
 
 public class ServerTest {
 	public static void main(String[] args) {
-		MyPaxos server = new MyPaxos("./conf/conf.json");
-		server.setGroupId(1, new KvCallback());
-		server.setGroupId(2, new KvCallback());
 		try {
+			MyPaxos server = new MyPaxos("./conf/conf.json");
+			server.setGroupId(1, new KvCallback());
+			server.setGroupId(2, new KvCallback());
 			server.start();
 		} catch (IOException | InterruptedException e) {
 			// TODO Auto-generated catch block
