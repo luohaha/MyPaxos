23224daa4e0354be292c208e4e2d349ab367f8b7
Yixin Luo <18810541851@163.com>
2017-04-29 16:12:15 +0800

    新增非阻塞发送客户端

---
+++ MyPaxos/src/main/java/com/github/luohaha/paxos/utils/NonBlockClientImpl.java
@@ -0,0 +1,111 @@
+package com.github.luohaha.paxos.utils;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class NonBlockClientImpl implements CommClient {
+
+	private Selector selector;
+	private ConcurrentMap<SocketChannel, ByteBuffer> channelMap;
+	private BlockingQueue<SocketChannel> channalQueue;
+
+	public NonBlockClientImpl() throws IOException {
+		super();
+		this.selector = Selector.open();
+		this.channelMap = new ConcurrentHashMap<>();
+		this.channalQueue = new LinkedBlockingQueue<>();
+		new Thread(() -> {
+			while (true) {
+				try {
+					this.selector.select();
+					SocketChannel newChan = this.channalQueue.poll();
+					if (newChan != null) {
+						// 注册新的事件
+						newChan.register(this.selector, SelectionKey.OP_CONNECT);
+					}
+					Set<SelectionKey> keys = this.selector.selectedKeys();
+					Iterator<SelectionKey> iterator = keys.iterator();
+					while (iterator.hasNext()) {
+						SelectionKey key = iterator.next();
+						SocketChannel channel = (SocketChannel) key.channel();
+						if (key.isConnectable()) {
+							if (channel.finishConnect()) {
+								channel.register(this.selector, SelectionKey.OP_WRITE);
+							} else {
+								throw new IOException();
+							}
+						} else if (key.isWritable()) {
+							// 数据可写
+							writeData(channel);
+						}
+						iterator.remove();
+					}
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
+
+	/**
+	 * 向channel中非阻塞地写入数据
+	 * 
+	 * @param channel
+	 * @throws IOException
+	 */
+	private void writeData(SocketChannel channel) throws IOException {
+		ByteBuffer buffer = this.channelMap.get(channel);
+		if (buffer == null) {
+			return;
+		}
+		if (buffer.hasRemaining()) {
+			channel.write(buffer);
+		}
+		if (!buffer.hasRemaining()) {
+			this.channelMap.remove(channel);
+			channel.close();
+		}
+	}
+
+	@Override
+	public void sendTo(String ip, int port, byte[] msg) throws UnknownHostException, IOException {
+		// TODO Auto-generated method stub
+		ByteBuffer buffer = ByteBuffer.allocate(msg.length);
+		buffer.put(msg);
+		buffer.flip();
+		SocketChannel socketChannel = SocketChannel.open();
+		socketChannel.configureBlocking(false);
+		this.channelMap.put(socketChannel, buffer);
+		this.channalQueue.add(socketChannel);
+		// 建立连接
+		SocketAddress address = new InetSocketAddress(ip, port);
+		socketChannel.connect(address);
+		this.selector.wakeup();
+	}
+
+	public static void main(String[] args) {
+		try {
+			CommClient client = new NonBlockClientImpl();
+			client.sendTo("localhost", 8888, new String("hello ").getBytes());
+			client.sendTo("localhost", 8888, new String("world!").getBytes());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
