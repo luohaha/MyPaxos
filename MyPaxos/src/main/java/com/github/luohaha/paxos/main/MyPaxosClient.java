2100176aa7921c9b37e46e291ed56df1c5a974f5
Yixin Luo <18810541851@163.com>
2017-06-03 17:54:28 +0800

    增加buffer，极大优化性能

--- MyPaxos/src/main/java/com/github/luohaha/paxos/main/MyPaxosClient.java
+++ MyPaxos/src/main/java/com/github/luohaha/paxos/main/MyPaxosClient.java
@@ -2,13 +2,21 @@ package com.github.luohaha.paxos.main;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
 import com.github.luohaha.paxos.core.WorkerType;
+import com.github.luohaha.paxos.exception.PaxosClientNullAddressException;
 import com.github.luohaha.paxos.packet.Packet;
 import com.github.luohaha.paxos.packet.PacketBean;
-import com.github.luohaha.paxos.utils.ClientImplByLC4J;
-import com.github.luohaha.paxos.utils.CommClient;
-import com.github.luohaha.paxos.utils.CommClientImpl;
+import com.github.luohaha.paxos.packet.Value;
+import com.github.luohaha.paxos.utils.client.ClientImplByLC4J;
+import com.github.luohaha.paxos.utils.client.CommClient;
+import com.github.luohaha.paxos.utils.client.CommClientImpl;
+import com.github.luohaha.paxos.utils.serializable.ObjectSerialize;
+import com.github.luohaha.paxos.utils.serializable.ObjectSerializeImpl;
 import com.google.gson.Gson;
 
 public class MyPaxosClient {
@@ -18,18 +26,59 @@ public class MyPaxosClient {
 	private int port;
 	// comm client
 	private CommClient commClient;
+	// buffer's size
+	private int bufferSize = 8;
+	// buffer
+	private Queue<byte[]> buffer; 
 	
-	private Gson gson = new Gson();
+	private int tmp = 0;
 	
-	public MyPaxosClient(String host, int port) throws IOException {
+	private ObjectSerialize objectSerialize = new ObjectSerializeImpl();
+	
+	public MyPaxosClient() throws IOException {
 		super();
-		this.host = host;
-		this.port = port;
 		this.commClient = new ClientImplByLC4J(1);
+		this.buffer = new ArrayDeque<>();
 	}
 	
-	public void submit(String value, int groupId) throws UnknownHostException, IOException {
-		Packet packet = new Packet(new PacketBean("SubmitPacket", value), groupId, WorkerType.SERVER);
-		this.commClient.sendTo(this.host, this.port, gson.toJson(packet).getBytes());
+	/**
+	 * 设置发送缓冲区大小
+	 * @param bufferSize
+	 */
+	public void setSendBufferSize(int bufferSize) {
+		this.bufferSize = bufferSize;
+	}
+	
+	/**
+	 * 设置对端地址
+	 * @param host
+	 * @param port
+	 */
+	public void setRemoteAddress(String host, int port) {
+		this.host = host;
+		this.port = port;
+	}
+	
+	/**
+	 * 刷新buffer
+	 * @param groupId
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
+	public void flush(int groupId) throws UnknownHostException, IOException {
+		if (this.buffer.isEmpty())
+			return;
+		Packet packet = new Packet(new PacketBean("SubmitPacket", new Value(tmp++, this.objectSerialize.objectToObjectArray(this.buffer))), groupId, WorkerType.SERVER);
+		this.commClient.sendTo(this.host, this.port, this.objectSerialize.objectToObjectArray(packet));
+		this.buffer.clear();
+	}
+	
+	public void submit(byte[] value, int groupId) throws PaxosClientNullAddressException, UnknownHostException, IOException {
+		if (this.host == null)
+			throw new PaxosClientNullAddressException();
+		this.buffer.add(value);
+		if (this.buffer.size() >= this.bufferSize) {
+			flush(groupId);
+		}
 	}
 }
